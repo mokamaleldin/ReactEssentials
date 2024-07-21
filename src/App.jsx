ca1292b004852afef2e2ebb4309cecbf5958b240
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-07-21 19:32:43 +0300

    chore: Add lazy loading for images and fix typo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
+import reactImg from "./assets/react-core-concepts.png"
+import componentsImg from './assets/components.png'
+
+const reactDescriptions = ['Fundamental', "Crucial", "Core"]
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const des = reactDescriptions[genRandomInt(2)];
+  setInterval(des, 1000)
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={ reactImg } alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        { des } React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
+function CoreConcept({ title, des, image }) {
+  return (
+    <li>
+      <img src={ image } alt="image" />
+      <h3>{ title }</h3>
+      <p>{ des }</p>
+    </li>
+  )
+}
+
 function App() {
   return (
-    <div>
+    <div> 
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Coer Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="mkee"
+              des="mkee is the best"
+              image={ componentsImg }
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
