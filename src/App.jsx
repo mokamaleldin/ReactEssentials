c2ee2c3e4c5cbea268fce354628da5b8a78b15e9
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-07-21 20:38:37 +0300

    chore: Refactor App.jsx to use CoreConceptsData and import Header and CoreConcept components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png"
-import componentsImg from './assets/components.png'
-
-const reactDescriptions = ['Fundamental', "Crucial", "Core"]
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header() {
-  const des = reactDescriptions[genRandomInt(2)];
-  setInterval(des, 1000)
-  return (
-    <header>
-      <img src={ reactImg } alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        { des } React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ title, des, image }) {
-  return (
-    <li>
-      <img src={ image } alt="image" />
-      <h3>{ title }</h3>
-      <p>{ des }</p>
-    </li>
-  )
-}
+import { CoreConceptsData } from './data'
+import Header from "./components/Header/Header"
+import { CoreConcept } from "./components/CoreConcept";
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -41,10 +12,20 @@ function App() {
           <h2>Coer Concepts</h2>
           <ul>
             <CoreConcept
-              title="mkee"
-              des="mkee is the best"
-              image={ componentsImg }
+              { ...CoreConceptsData[0] }
             />
+            <CoreConcept
+              { ...CoreConceptsData[1] }
+            />
+            <CoreConcept
+              { ...CoreConceptsData[2] }
+            />
+            <CoreConcept
+              title={ CoreConceptsData[3].title }
+              description={ CoreConceptsData[3].description }
+              image={ CoreConceptsData[3].image }
+            />
+
           </ul>
         </section>
       </main>
