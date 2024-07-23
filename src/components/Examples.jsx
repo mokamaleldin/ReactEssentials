780c1d8dd796ed0bd0cafa7625ad618b5e8b5013
mokamalelidn <mhmdalknj496@gmail.com>
2024-07-23 09:40:07 +0300

    chore: Refactor Examples.jsx to use Tabs component for improved code organization

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,22 +2,24 @@ import { EXAMPLES } from '../data'
 import { useState } from 'react';
 import TabButton from './TabButton';
 import Section from './Section';
+import Tabs from './Tabs';
 
 export default function Examples() {
     const [tabContent, setTabContent] = useState(1);
 
     function handleSelect(selctedButton) {
         setTabContent(selctedButton);
-    }
+    }    
     return (
         <Section title={ 'Examples' } id="examples">
-            <menu>
-                
-                <TabButton isSelected={ tabContent === 1 } onClick={ () => handleSelect(1) }>components</TabButton>
-                <TabButton isSelected={ tabContent === 2 } onClick={ () => handleSelect(2) }>JSX</TabButton>
-                <TabButton isSelected={ tabContent === 3 } onClick={ () => handleSelect(3) }>Props</TabButton>
-                <TabButton isSelected={ tabContent === 4 } onClick={ () => handleSelect(4) }>State</TabButton>
-            </menu>
+            <Tabs
+                buttons={ <>
+                    <TabButton isSelected={ tabContent === 1 } onClick={ () => handleSelect(1) }>components</TabButton>
+                    <TabButton isSelected={ tabContent === 2 } onClick={ () => handleSelect(2) }>JSX</TabButton>
+                    <TabButton isSelected={ tabContent === 3 } onClick={ () => handleSelect(3) }>Props</TabButton>
+                    <TabButton isSelected={ tabContent === 4 } onClick={ () => handleSelect(4) }>State</TabButton>
+                </> }>
+
             { !tabContent ? <p>Select a tab to view the content</p> :
                 <div id="tab-content">
                     <h3>{ EXAMPLES[tabContent].title }</h3>
@@ -28,6 +30,9 @@ export default function Examples() {
                         </code>
                     </pre>
                 </div> }
+            </Tabs>
+
+
         </Section>
     )
 }
